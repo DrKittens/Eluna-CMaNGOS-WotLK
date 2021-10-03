83e2c2749bace42874ce797042c7a055ae0e0589
Ethan <ebakkedahl@gmail.com>
2021-10-03 02:50:26 -0500

    Add Eluna LUA Engine
    -Add Eluna Lua Engine support to core
    -Add Eluna Submodule to repository
    -Add CI check to include Eluna

--- src/game/Spells/SpellMgr.h
+++ src/game/Spells/SpellMgr.h
@@ -2736,6 +2736,8 @@ inline bool IsProfessionOrRidingSkill(uint32 skill)
     return  IsProfessionSkill(skill) || skill == SKILL_RIDING;
 }
 
+typedef std::vector<std::unique_ptr<SpellEntry>> SpellEntryMap;
+
 class SpellMgr
 {
         friend struct DoSpellBonuses;
@@ -3329,6 +3331,9 @@ class SpellMgr
         void LoadPetDefaultSpells();
         void LoadSpellAreas();
 
+        SpellEntry const* GetSpellEntry(uint32 spellId) const { return spellId < GetMaxSpellId() ? mSpellEntryMap[spellId].get() : nullptr; }
+        uint32 GetMaxSpellId() const { return mSpellEntryMap.size(); }
+
     private:
         bool LoadPetDefaultSpells_helper(CreatureInfo const* cInfo, PetDefaultSpellsEntry& petDefSpells);
 
@@ -3351,6 +3356,8 @@ class SpellMgr
         SpellAreaMap         mSpellAreaMap;
         SpellAreaForAuraMap  mSpellAreaForAuraMap;
         SpellAreaForAreaMap  mSpellAreaForAreaMap;
+
+        SpellEntryMap      mSpellEntryMap;
 };
 
 #define sSpellMgr SpellMgr::Instance()
